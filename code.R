cb754a5aada5b174a12512f3f5545db0189d2f09
AGregorc <anze.gregorc44@gmail.com>
2017-12-03 00:01:58 +0100

    Summary of data

--- code.R
+++ code.R
@@ -4,4 +4,17 @@
 #
 ##############################################################################
 
+# To read data from a text file, use the "read.table" command.
+# The parameter header=TRUE indicates that the file to be read includes a first line with the column names
+md <- read.table(file="dataSem1.txt", sep=",", header=TRUE)
 
+# Summarize data
+summary(md)
+# Get all header names
+names(md)
+# Number of rows
+nrow(md)
+# Number of columns
+ncol(md)
+# First 6 rows in data
+head(md)
