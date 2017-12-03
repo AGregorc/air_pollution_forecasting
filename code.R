29f20a9e935209764e3e6036c9a6a3821145e869
AGregorc <anze.gregorc44@gmail.com>
2017-12-03 15:24:59 +0100

    Constructing new attributes

--- code.R
+++ code.R
@@ -18,3 +18,47 @@ nrow(md)
 ncol(md)
 # First 6 rows in data
 head(md)
+
+
+# Useful data visualization functions
+plot(md$Glob_radiation_mean)
+hist(md$Glob_radiation_mean)
+plot(density(md$Glob_radiation_mean))
+boxplot(md$Glob_radiation_mean)
+barplot(table(md$Glob_radiation_mean))
+
+##############################################################################
+#
+# ATTRIBURE EVALUATION AND CONSTRUCTING NEW ATTRIBUTES
+#
+##############################################################################
+
+#install.packages('dummies')
+library(dummies)
+
+
+# Convert Site attribute with one hot encoding
+md <- dummy.data.frame(md, names=c("Site"), sep="_")
+
+
+# library to work with date-time
+# install.packages('lubridate')
+library(lubridate)
+
+date <- md$Date
+md$Date <- NULL
+
+month(as.POSIXlt(date, format="%Y-%m-%d"))
+a <- data.frame(wday(as.POSIXlt(date, format="%Y-%m-%d"), label=TRUE))
+colnames(a) <- c("Dan")
+names(a)
+
+a <- dummy.data.frame(a, names=c("Dan"), sep="_")
+names(a)
+
+md[names(a)] <- a
+
+
+
+
+
