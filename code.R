4d83da67ed941dde047753a396f5f39119d2633c
AGregorc <anze.gregorc44@gmail.com>
2017-12-10 15:46:19 +0100

    Added prediction models for PM10

--- code.R
+++ code.R
@@ -3,9 +3,11 @@
 #install.packages('dummies')
 #install.packages('lubridate')
 #install.packages("CORElearn")  
+#install.packages('nnet')
 library(dummies)
 library(lubridate) # library to work with date-time
 library(CORElearn)
+library(nnet)
 source("myfunctions.R")
 
 ##############################################################################
@@ -104,7 +106,7 @@ att <- sort(attrEval(ozone ~ ., learn, "InfGain"), decreasing = TRUE)
 att <- head(att, 2) # best n attributes
 set <- learn[names(att)]
 
-# TRAINING
+# TRAINING THE MAX OZONE LEVEL (O3)
 
 # build a decision tree using information gain as a splitting criterion
 modelDT <- CoreModel(ozone  ~ ., set, model="tree", selectionEstimator="InfGain")
@@ -112,8 +114,9 @@ modelDT <- CoreModel(ozone  ~ ., set, model="tree", selectionEstimator="InfGain"
 
 modelNB <- CoreModel(ozone ~ ., set, model="bayes")
 modelKNN <- CoreModel(ozone ~ ., set, model="knn", kInNN = 5)
+modelRF <- CoreModel(ozone ~ ., data = set, model="rf") # Random forest
 
-# TESTING
+# TESTING O3
 test.ozone <- getOzoneLevel(test$O3)
 
 predDT <- predict(modelDT, test, type = "class")
@@ -127,8 +130,45 @@ predKNN <- predict(modelKNN, test, type="class")
 caKNN <- CA(test.ozone, predKNN)
 caKNN
 
+predRF <- predict(modelRF, test, type="class")
+caRF <- CA(test.ozone, predRF)
+caRF
+
 # Combined results
-pred <- data.frame(predDT, predNB, predKNN, test.ozone)
+pred <- data.frame(predDT, predNB, predKNN, predRF, test.ozone)
+pred
+
+# TRAINING the concentration of large pollution particles (PM10)
+
+# build a decision tree using information gain as a splitting criterion
+modelDT <- CoreModel(PM10  ~ ., set, model="tree", selectionEstimator="InfGain")
+#plot(modelDT, set)
+
+modelNB <- CoreModel(PM10 ~ ., set, model="bayes")
+modelKNN <- CoreModel(PM10 ~ ., set, model="knn", kInNN = 5) 
+modelRF <- CoreModel(PM10 ~ ., data = set, model="rf") # Random forest
+
+# TESTING PM10
+test.PM10 <- getPM10classes(test$PM10)
+
+predDT <- predict(modelDT, test, type = "class")
+caDT <- CA(test.PM10, predDT)
+
+predNB <- predict(modelNB, test, type="class")
+caNB <- CA(test.PM10, predNB)
+caNB
+
+predKNN <- predict(modelKNN, test, type="class")
+caKNN <- CA(test.PM10, predKNN)
+caKNN
+
+predRF <- predict(modelRF, test, type="class")
+caRF <- CA(test.PM10, predRF)
+caRF
+
+
+# Combined results
+pred <- data.frame(predDT, predNB, predKNN, predRF, test.PM10)
 pred
 
 
